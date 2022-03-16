cee45c49e65975ec03ea2c1d582684d2c4bc5589
yyeopp <letsksy@naver.com>
2022-03-16 23:34:43 +0900

    0316

--- DB/MySQL.md
+++ DB/MySQL.md
@@ -18,8 +18,6 @@
   
   - 여러 테이블에 분산되어 있는 데이터를 검색 시 테이블 간의 관계(join)를 이용하여 필요한 데이터를 검색.
 
-
-
 ### SQL (Structured Query Language)
 
 #### SQL
@@ -36,12 +34,131 @@
 
 - insert, update, delete, select, create, alter, drop, rename, commit, rollback, grant, revoke 까지
 
-
-
 ### Data Type
 
-#### table 생성
+#### 문자형
 
 - CHAR: 고정 길이를 갖는 문자열
 
 - VARCHAR: 가변 길이를 갖는 문자열
+
+- TEXT: 최대 65535 byte
+
+- 그 외 tinytext, mediumtext, longtext, enum(열거형), set(집합형)
+
+#### 숫자형
+
+#### 날짜형
+
+#### 이진 데이터 타입
+
+### table 생성
+
+## DML (Insert, Update, Delete)
+
+### Insert
+
+### Update
+
+### Delete
+
+## DML(Select)
+
+### Diagram
+
+### 기본 Select
+
+### alias, 사칙연산, NULL
+
+### CASE, when, END
+
+### Where
+
+`Select, from, where` 가 주어질 때 실행 순서는?
+
+- from -> where -> select
+
+### And, or, not
+
+### In
+
+### Between
+
+### Null 비교
+
+### Like
+
+### Order by
+
+`Select, from, where, order by`가 주어질 때 실행 순서는?
+
+- from -> where -> select -> order by
+
+- 여러 기준으로 정렬 가능: 순차적으로 이루어짐
+
+## 내장함수
+
+### MySQL 내장함수
+
+- 숫자 관련, 문자 관련, 날짜 관련, 논리 관련, 그룹 함수
+
+
+
+## Transaction
+
+- 데이터베이스의 상태를 변화시키는 일종의 작업 단위
+
+- 여러 도구들이 존재
+  
+  - commit, rollback
+
+
+
+## Aggregation
+
+## Group by Clause
+
+### group by 절
+
+- select 문에서 group by 절을 사용할 경우 DB는 쿼리된 테이블의 행을 **그룹으로 묶는다.**
+
+- DB는 선택 목록의 집계 함수를 각 행 그룹에 적용하고 각 그룹에 대해 단일 결과 행을 반환한다.
+
+- group by 절을 생략하면 DB는 선택목록의 집계 함수를 쿼리된 테이블의 모든 행에 적용한다.
+
+- select 절의 모든 요소는 group by 절의 표현식, 집계 함수를 포함하는 표현식 또는 상수만 가능
+
+- 형식
+  
+  ```sql
+  select columns
+  from table_name
+  where conditions
+  group by grouping columns
+  having grouping conditions
+  order by col [ASC | DESC]
+  ```
+  
+  **실행순서**는 512346. 꼭 외우기!
+
+
+
+### having 절
+
+- group by한 결과에 조건을 추가
+
+- 쿼리의 실행 순서를 보면 where절이 group by절보다 먼저 실행되기 때문에 **aggregate 조건은 having절에 작성.**
+  
+  - where는 일반조건, having은 그룹조건.
+
+- order  by는 가장 마지막에 실행되므로, 그룹함수가 사용된 상황에서도 별 문제없이 넣을 수 있다.
+
+
+
+## Set Operator
+
+### Set (집합연산자)
+
+- 모든 집합 연산자는 동일한 우선순위를 갖는다.
+
+- select 절에 있는 column의 개수와 type이 일치해야 한다.
--- FrontEnd/JavaScript/practice/main.js
+++ FrontEnd/JavaScript/practice/main.js
@@ -9,70 +9,103 @@ $(function () {
   $("#input-date").append(`${year}년 ${month}월`);
 
   // 지도 세팅
+  var map;
+  const defaultPos = [37.5012743, 127.039585];
+  const defaultNm = "멀티캠퍼스 역삼";
+  var markers = [];
+  var markerPositions = [];
 
   // 지도 생성
   var container = document.getElementById("map");
   var options = {
     // 기본 위치 멀티캠퍼스
-    center: new kakao.maps.LatLng(37.5012743, 127.039585),
-    level: 3,
+    center: new kakao.maps.LatLng(defaultPos[0], defaultPos[1]),
+    level: 6,
   };
   map = new kakao.maps.Map(container, options);
 
   // 마커 생성
-  var markerPosition = new kakao.maps.LatLng(37.5012743, 127.039585);
+  var markerPosition = new kakao.maps.LatLng(defaultPos[0], defaultPos[1]);
   var marker = new kakao.maps.Marker({
     position: markerPosition,
   });
+  markers.push(marker);
   marker.setMap(map);
 
   // 인포윈도우 생성
-  var iwContent =
-      '<div style="padding:5px;">멀티캠퍼스 <br><a href="https://map.kakao.com/link/map/멀티캠퍼스,37.5012743, 127.039585" style="color:blue" target="_blank">큰지도보기</a> <a href="https://map.kakao.com/link/to/멀티캠퍼스,37.5012743, 127.039585" style="color:blue" target="_blank">길찾기</a></div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-    iwPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+  var iwContent = "",
+    iwPosition = new kakao.maps.LatLng(defaultPos[0], defaultPos[1]);
+
   var infowindow = new kakao.maps.InfoWindow({
     position: iwPosition,
     content: iwContent,
   });
-  infowindow.open(map, marker);
+  // infowindow.open(map, marker);
 
   // geolocation으로 페이지 로딩 시 지도의 위치를 사용자 위치로 설정
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
-      var lat = position.coords.latitude,
-        lon = position.coords.longitude;
+      var lat = position.coords.latitude;
+      var lon = position.coords.longitude;
+      mapPosition = new kakao.maps.LatLng(lat, lon);
+      marker = new kakao.maps.Marker({
+        position: mapPosition,
+        clickable: true,
+      });
 
-      var locPosition = new kakao.maps.LatLng(lat, lon),
-        // 인포윈도우
-        message = '<div style="padding:5px;">여기에 계신가요?!</div>';
-
-      // 마커와 인포윈도우 표시
-      displayMarker(locPosition, message);
+      map.setCenter(mapPosition);
+      marker.setMap(map);
+      markers.push(marker);
     });
   } else {
     // geolocation 실패 시 초기 주소로 이동
-    var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),
-      message = "geolocation을 사용할수 없어요..";
-    displayMarker(locPosition, message);
+    moveMap(defaultPos, defaultNm);
   }
 
-  // 지도에 마커와 인포윈도우 표시
-  function displayMarker(locPosition, message) {
-    var marker = new kakao.maps.Marker({
-      map: map,
-      position: locPosition,
+  function moveMap(newPos, bdNm) {
+    mapPosition = new kakao.maps.LatLng(newPos[0], newPos[1]);
+    marker = new kakao.maps.Marker({
+      position: mapPosition,
+      clickable: true,
     });
 
-    var iwContent = message,
-      iwRemoveable = true;
+    map.setCenter(mapPosition);
+    marker.setMap(map);
+    markers.push(marker);
+    markerPositions.push(newPos);
+    kakao.maps.event.addListener(marker, "click", genInfoWin(marker, bdNm));
+  }
 
-    var infowindow = new kakao.maps.InfoWindow({
-      content: iwContent,
-      removable: iwRemoveable,
-    });
+  function genInfoWin(marker, bdNm) {
+    return function () {
+      iwContent = bdNm;
+      infowindow = new kakao.maps.InfoWindow({
+        content: iwContent,
+        removable: true,
+      });
+      infowindow.open(map, marker);
+    };
+  }
 
-    infowindow.open(map, marker);
-    map.setCenter(locPosition);
+  $("#map-header-btn").on("click", function () {
+    removeMarkers();
+  });
+
+  // 마커를 전부 지우는 함수
+  function removeMarkers() {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
+    }
+    markers.length = 0;
+    markerPositions.length = 0;
+
+    let inputDate = "";
+    if (month >= 10) {
+      inputDate = `${year}${month}`;
+    } else {
+      inputDate = `${year}0${month}`;
+    }
+    initiateByDate(inputDate);
   }
 
   // 로딩과 함께 시-도 정보는 불러놓기
@@ -100,6 +133,10 @@ $(function () {
 
   // 시/도 선택 시 시/군/구 더블셀렉트 시작
   $("#city-select").on("change", function () {
+    doubleSelect();
+  });
+
+  function doubleSelect() {
     let selectedCityCode = $("#city-select").find(":checked").val();
 
     let regionData = {
@@ -116,31 +153,57 @@ $(function () {
 
       success: function (resource) {
         $("#region-select").empty().append("<option>==시/군/구==</option>");
-        let i = 0;
-        $.each(resource.regcodes, function () {
-          let regionName = $(this)[0].name;
+        let responsedCodes = resource.regcodes;
+
+        // 성남시 분당구 같은 예외 케이스를 위한 알고리즘 문제 풀이 시작
+        let validResources = [];
+
+        let presentReg = responsedCodes[0].code.substring(0, 4);
+        let presentRegBig = responsedCodes[0].code.substring(4, 5);
+        for (let i = 1; i < responsedCodes.length; i++) {
+          let temp = responsedCodes[i].code.substring(0, 4);
+          if (presentReg != temp) {
+            validResources.push(responsedCodes[i - 1]);
+            presentReg = temp;
+            presentRegBig = responsedCodes[i].code.substring(4, 5);
+            continue;
+          } else {
+            if (presentRegBig == "0") {
+              presentRegBig = responsedCodes[i].code.substring(4, 5);
+              continue;
+            } else {
+              validResources.push(responsedCodes[i - 1]);
+              presentRegBig = responsedCodes[i].code.substring(4, 5);
+            }
+          }
+        }
+        validResources.push(responsedCodes[responsedCodes.length - 1]);
+
+        $.each(validResources, function () {
+          let temp = $(this)[0].name.split(" ");
+          let regionName = "";
+          for (let i = 1; i < temp.length; i++) {
+            regionName += temp[i];
+            regionName += " ";
+          }
+          regionName.length = regionName.length - 1;
+
           let regionOption = ``;
-          // TODO: 성남시 분당구 같은거
 
           regionOption += `
-          <option value="${$(this)[0].code}">${regionName.split(" ")[1]}</option>
+          <option value="${$(this)[0].code}">${regionName}</option>
           `;
           $("#region-select").append(regionOption);
         });
-        $.each(resource.regcodes, function () {
-          let regionName = $(this)[0].name;
-          let regionOption = ``;
-          regionOption += `
-          <option value="${$(this)[0].code}">${regionName.split(" ")[1]}</option>
-          `;
-          $("#region-select").append(regionOption);
-        });
+
+        // 시도를 바꾸면 마커 다 지우기
+        removeMarkers();
       },
       error: function () {
         console.log("error");
       },
     });
-  });
+  }
 
   // 지역선택 완료 시 간단정보 출력
   $("#region-select").on("change", function () {
@@ -174,6 +237,7 @@ $(function () {
     }
 
     initiateByDate(inputDate);
+    removeMarkers();
   });
 
   $("#next-date").on("click", function () {
@@ -194,12 +258,16 @@ $(function () {
     }
 
     initiateByDate(inputDate);
+    removeMarkers();
   });
 
   function initiateByDate(inputDate) {
     //TODO
+    if ($("#region-select").find(":checked").val() == "==시/군/구==") {
+      // console.log("Return");
+      return;
+    }
     let selectedRegionCode = $("#region-select").find(":checked").val().substring(0, 5);
-
     let requestData = {
       serviceKey:
         "7NDGKNSJ36Y6ap6Iy7q9TRUlxBRBlCy0OgV0U8EAlXYH0MXVrAGSZlQXPJefiTP8j/ZFSzJ6jVXJIIRkOipOlw==",
@@ -217,11 +285,13 @@ $(function () {
       dataType: "xml",
       success: function (response) {
         // 지도 제목에 선택한 위치 써놓기
+        // console.log("requested");
         let regionName = "";
         if ($("#city-select").find(":checked").text() == "==시/도==") {
           regionName = "";
         } else if ($("#region-select").find(":checked").text() == "==시/군/구==") {
           regionName = $("#city-select").find(":checked").text();
+          // console.log("여기???");
         } // 지역 선택이 완전히 이루어진 경우에만 정보 출력
         else {
           regionName = `
@@ -234,21 +304,6 @@ $(function () {
         }
         $("#map-header-region").empty().append(regionName);
 
-        // 테이블에 마우스 올릴 시의 css 속성 부여  ----> bootstrap 대체 예정
-        $("#apt-outline tr")
-          .mouseover(function () {
-            $(this).children().css({
-              backgroundColor: "#DCDCDC",
-              cursor: "pointer",
-            });
-          })
-          .mouseout(function () {
-            $(this).children().css({
-              backgroundColor: "#FFFFFF",
-              cursor: "default",
-            });
-          });
-
         // 테이블 선택 시 상세정보 출력하는 함수 호출
         makeDetailData(response);
       },
@@ -258,48 +313,147 @@ $(function () {
     });
   }
 
+  // 북마크들 저장
+  var bookmarks = [];
+
+  // 북마크 추가하기
+  $("#btn-bookmark").on("click", function () {
+    let cityName = $("#city-select").find(":checked").text();
+    let regName = $("#region-select").find(":checked").text();
+    if (regName == "==시/군/구==") {
+      alert("지역을 선택해주세요.");
+      return;
+    }
+    let regionName = `${cityName}<br> ${regName}`;
+
+    // 북마크 추가 유효성 검사
+    for (let i = 0; i < bookmarks.length; i++) {
+      if (bookmarks[i] == regionName) {
+        alert("이미 추가된 지역입니다.");
+        return;
+      }
+    }
+
+    bookmarks.push(regionName);
+    $("#region-bookmark").append(`
+      <tr>
+        <td>
+          <div class="row">
+            <div class="col-7" style="font-size: 12px">${regionName}</div>
+            <div class="col-2">
+              <input type="button" id="btn-bookmark-use" value="조회" style="font-size: 6px">
+            </div>
+            <div class="col-2">
+              <input type="button" id="btn-bookmark-del" value="삭제" style="font-size: 6px">
+            </div>
+          </div>
+        </td>
+      </tr>
+    `);
+
+    // 저장된 북마크를 눌러서 조회하기
+    $("#btn-bookmark-use").on("click", function () {
+      useBookmark($(this));
+    });
+  });
+
+  $("#btn-bookmark-del").on("click", function () {
+    delBookmark($(this));
+  });
+
+  //TDOD: 고장남
+  function useBookmark(button) {
+    cityName = button.parent().parent().children().eq(0).text().split(" ")[0];
+    regName = button.parent().parent().children().eq(0).text().split(" ")[1];
+    // select에 성공하게 만들면 되는데..
+    //$('select[name="options"]').find('option:contains("Blue")').attr("selected",true);
+    $("#city-select")
+      .children()
+      .each(function () {
+        if ($(this).text() == cityName) {
+          $(this).prop("selected", true);
+          doubleSelect();
+        }
+      });
+
+    $("#region-select")
+      .children()
+      .each(function () {
+        if ($(this).text() == regName) {
+          $(this).prop("selected", true);
+        }
+      });
+    // if (month >= 10) {
+    //   inputDate = `${year}${month}`;
+    // } else {
+    //   inputDate = `${year}0${month}`;
+    // }
+    // console.log($("#region-select").find(":checked").text());
+
+    // initiateByDate(inputDate);
+  }
+
+  // TODO: 고장남
+  function delBookmark(button) {
+    button.parent().parent().remove();
+  }
+
+  // 북마크 버튼으로 지역 조회
   // 지역 선택 완료 시 간략 정보 뿌리는 함수
   function makeRoughData(response) {
+    // console.log("여기?");
     let roughData = ``;
     roughData += `
+        <thead>  
           <tr>
             <th>아파트명</th>
             <th>거래일자</th>
-          </tr>`;
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+        `;
     $(response)
       .find("item")
       .each(function () {
         roughData += `
-              <tr>
+            <tr>
               <td>${$(this).find("아파트").text()}</td>
               <td>${$(this).find("년").text()}.${$(this).find("월").text()}.${$(this)
           .find("일")
-          .text()}</td></tr>
+          .text()}</td>
+              <td style="visibility: hidden">0</td>
+            </tr>
+            
               `;
       });
+    roughData += `</tbody>`;
 
     $("#apt-outline").empty().append(roughData);
   }
 
   // 상세정보를 뿌리는 함수
   function makeDetailData(response) {
-    $("#apt-outline tr").on("click", function () {
+    $("#apt-outline tbody tr").on("click", function () {
       let td = $(this).children();
 
-      let name = td.eq(0).text();
-      let date = td.eq(1).text();
+      // roughData 테이블의 행을 클릭했을 때 음영부여+버튼생성
+      if ($(this).children().eq(2).text() == 0) {
+        trClick(this);
+        let name = td.eq(0).text();
+        let date = td.eq(1).text();
 
-      $(response)
-        .find("item")
-        .each(function () {
-          if (
-            $(this).find("아파트").text() == name &&
-            `${$(this).find("년").text()}.${$(this).find("월").text()}.${$(this)
-              .find("일")
-              .text()}` == date
-          ) {
-            let detailData = ``;
-            detailData += `
+        $(response)
+          .find("item")
+          .each(function () {
+            if (
+              $(this).find("아파트").text() == name &&
+              `${$(this).find("년").text()}.${$(this).find("월").text()}.${$(this)
+                .find("일")
+                .text()}` == date
+            ) {
+              let detailData = ``;
+              detailData += `
             <tr>
               <th>이름</th>
               <th>거래금액</th>
@@ -312,23 +466,51 @@ $(function () {
               <td>${$(this).find("아파트").text()}</td>
               <td>${$(this).find("거래금액").text()}</td>
               <td>${$(this).find("년").text()}.${$(this).find("월").text()}.${$(this)
-              .find("일")
-              .text()}</td>
+                .find("일")
+                .text()}</td>
               <td>${$(this).find("전용면적").text()}</td>
               <td>${$(this).find("층").text()}</td>
               <td>${$(this).find("건축년도").text()}</td>
             </tr>
             `;
-            $("#apt-detailInfo").empty().append(detailData);
+              $("#apt-detailInfo").empty().append(detailData);
 
-            // 지도 구현을 위해 좌표를 구하는 함수 호출
-            findProjection($(this));
-          }
-        });
+              // 지도 구현을 위해 좌표를 구하는 함수 호출
+              findProjection($(this));
+            }
+          });
+      } else {
+        trDefault($(this));
+        let name = td.eq(0).text();
+        let date = td.eq(1).text();
+
+        $(response)
+          .find("item")
+          .each(function () {
+            if (
+              $(this).find("아파트").text() == name &&
+              `${$(this).find("년").text()}.${$(this).find("월").text()}.${$(this)
+                .find("일")
+                .text()}` == date
+            ) {
+              choosedMarkerRemoving($(this));
+            }
+          });
+      }
     });
   }
 
-  // 좌표를 호출하는 함수
+  function trClick(tr) {
+    $(tr).css("background-color", "bisque").css("font-weight", "bold");
+    $(tr).children().eq(2).empty().append(1);
+  }
+
+  function trDefault(tr) {
+    $(tr).css("background-color", "white").css("color", "black").css("font-weight", "");
+    $(tr).children().eq(2).empty().append(0);
+  }
+
+  // 좌표호출 + 지도 이동 + 마커 생성
   function findProjection(response) {
     let mapRequest = {
       confmKey: "devU01TX0FVVEgyMDIyMDMxMjIwMjEwOTExMjMzOTI=",
@@ -336,7 +518,48 @@ $(function () {
         .find("법정동읍면동코드")
         .text()}`,
       rnMgtSn: `${response.find("도로명시군구코드").text()}${response.find("도로명코드").text()}`,
-      udrtYn: response.find("도로명지상지하코드").text(),
+      udrtYn: "0",
+      buldMnnm: response.find("도로명건물본번호코드").text(),
+      buldSlno: response.find("도로명건물부번호코드").text(),
+    };
+
+    let bdNm = $(response).find("아파트").text();
+
+    $.ajax({
+      url: "https://www.juso.go.kr/addrlink/addrCoordApi.do",
+      type: "GET",
+      data: mapRequest,
+      dataType: "xml",
+      success: function (response) {
+        let entX = Math.round($(response).find("entX").text());
+        let entY = Math.round($(response).find("entY").text());
+
+        let grs80 =
+          "+proj=tmerc +lat_0=38 +lon_0=127.5 +k=0.9996 +x_0=1000000 +y_0=2000000 +ellps=GRS80 +units=m +no_defs";
+        let wgs84 =
+          "+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees";
+
+        let temp = proj4(grs80, wgs84).forward([entX, entY]);
+        let newPos = [temp[1], temp[0]];
+
+        // 해당 좌표값으로 지도를 이동시키고 마커찍기
+        moveMap(newPos, bdNm);
+      },
+      error: function () {
+        console.log(error);
+      },
+    });
+  }
+
+  // 좌표 호출 + 이미 존재하던 마커 삭제
+  function choosedMarkerRemoving(response) {
+    let mapRequest = {
+      confmKey: "devU01TX0FVVEgyMDIyMDMxMjIwMjEwOTExMjMzOTI=",
+      admCd: `${response.find("법정동시군구코드").text()}${response
+        .find("법정동읍면동코드")
+        .text()}`,
+      rnMgtSn: `${response.find("도로명시군구코드").text()}${response.find("도로명코드").text()}`,
+      udrtYn: "0",
       buldMnnm: response.find("도로명건물본번호코드").text(),
       buldSlno: response.find("도로명건물부번호코드").text(),
     };
@@ -349,19 +572,20 @@ $(function () {
       success: function (response) {
         let entX = Math.round($(response).find("entX").text());
         let entY = Math.round($(response).find("entY").text());
-        console.log(entX);
-        console.log(entY);
-
-        // TODO: 좌표가 한번씩 뻑나는데???
 
         let grs80 =
           "+proj=tmerc +lat_0=38 +lon_0=127.5 +k=0.9996 +x_0=1000000 +y_0=2000000 +ellps=GRS80 +units=m +no_defs";
         let wgs84 =
           "+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees";
 
-        let newPos = proj4(grs80, wgs84).forward([entX, entY]);
-        console.log(newPos);
-        // 해당 좌표값으로 지도를 이동시키고 마커찍기
+        let temp = proj4(grs80, wgs84).forward([entX, entY]);
+        let deletePos = [temp[1], temp[0]];
+
+        for (let i = 0; i < markerPositions.length; i++) {
+          if (deletePos[0] == markerPositions[i][0] && deletePos[1] == markerPositions[i][1]) {
+            markers[i].setMap(null);
+          }
+        }
       },
       error: function () {
         console.log(error);
