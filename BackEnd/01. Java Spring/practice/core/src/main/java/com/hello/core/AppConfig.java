5be4262b4fcd64e0e7a5a6f4699b5543dea1538c
yyeopp <letsksy@naver.com>
2022-10-29 17:12:28 +0900

    [1029]

--- BackEnd/01. Java Spring/practice/core/src/main/java/com/hello/core/AppConfig.java
+++ BackEnd/01. Java Spring/practice/core/src/main/java/com/hello/core/AppConfig.java
@@ -8,26 +8,31 @@ import com.hello.core.member.MemberServiceImpl;
 import com.hello.core.member.MemoryMemberRepository;
 import com.hello.core.order.OrderService;
 import com.hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(discountPolicy(), memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
-
-
 
 }
