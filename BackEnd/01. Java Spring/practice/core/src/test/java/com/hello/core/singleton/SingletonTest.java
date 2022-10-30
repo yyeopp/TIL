eb4c7edaaf90bdf807e375ff713beb5e93bea24c
yyeopp <letsksy@naver.com>
2022-10-30 21:59:17 +0900

    [1030]

--- BackEnd/01. Java Spring/practice/core/src/test/java/com/hello/core/singleton/SingletonTest.java
+++ BackEnd/01. Java Spring/practice/core/src/test/java/com/hello/core/singleton/SingletonTest.java
@@ -4,6 +4,8 @@ import com.hello.core.AppConfig;
 import com.hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
 import static org.assertj.core.api.Assertions.*;
@@ -33,4 +35,16 @@ public class SingletonTest {
 
         assertThat(singletonService1).isSameAs(singletonService2);
     }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        assertThat(memberService1).isSameAs(memberService2);
+    }
+
+
 }
