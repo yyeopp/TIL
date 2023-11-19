26cf7d6cd2514d552cd5d88631bdaf75e5595947
yyeopp <letsksy@naver.com>
2023-11-19 17:27:29 +0900

    [1119] Unchecked Exception

---
+++ BackEnd/05. Spring DB/practice/jdbc/src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
@@ -0,0 +1,65 @@
+package hello.jdbc.exception.basic;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.ConnectException;
+import java.sql.SQLException;
+
+public class UncheckedAppTest {
+
+    static class Service {
+        Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
+
+        @Test
+        void unchecked() {
+            Controller controller = new Controller();
+            Assertions.assertThatThrownBy(controller::request)
+                    .isInstanceOf(RuntimeSQLException.class);
+        }
+        static class Controller {
+            Service service = new Service();
+            public void request() {
+                service.logic();
+
+            }
+        }
+
+        public void logic() {
+            repository.call();
+            networkClient.call();
+
+        }
+    }
+    static class NetworkClient {
+        public void call () {
+            throw new RuntimeConnectException("연결 실패");
+        }
+    }
+    static class Repository {
+        public void call() {
+            try {
+                runSQL();
+            } catch (SQLException e) {
+                throw new RuntimeSQLException(e);
+            }
+        }
+
+        public void runSQL() throws SQLException {
+            throw new SQLException("ex");
+        }
+    }
+
+    static class RuntimeConnectException extends RuntimeException {
+        public RuntimeConnectException(String message) {
+            super(message);
+        }
+    }
+
+    static class RuntimeSQLException extends RuntimeException {
+        public RuntimeSQLException(Throwable cause) {
+            super(cause);
+        }
+    }
+}
