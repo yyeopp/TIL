2398a78dc3afa289af01c4bc33abd2f7753e1afe
yyeopp <letsksy@naver.com>
2024-04-06 19:42:51 +0900

    [0406] ThreadLocal

---
+++ BackEnd/07. Spring Advanced/practice/advanced/src/main/java/hello/advanced/trace/TraceId.java
@@ -0,0 +1,41 @@
+package hello.advanced.trace;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.UUID;
+
+@Getter
+@Slf4j
+public class TraceId {
+
+    private String id;
+    private int level;
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private TraceId(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    private String createId() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    public TraceId createNextId() {
+        return new TraceId(id, level + 1);
+    }
+
+    public TraceId createPreviousId() {
+        return new TraceId(id, level - 1);
+    }
+
+    public boolean isFirstLevel() {
+        return level == 0;
+    }
+
+}
---
+++ BackEnd/07. Spring Advanced/practice/advanced/src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,44 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private ThreadLocalService threadLocalService = new ThreadLocalService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> {
+            threadLocalService.logic("userA");
+        };
+
+        Runnable userB = () -> {
+            threadLocalService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+//        sleep(2000);        // 동시성 문제 차단
+        threadB.start();
+        sleep(3000 );
+        log.info("main exit");
+
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
