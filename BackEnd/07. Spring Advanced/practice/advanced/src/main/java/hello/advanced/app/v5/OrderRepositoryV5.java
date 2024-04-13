5a3e902f4673662996f43b67c23eb923a1ea1ad1
yyeopp <letsksy@naver.com>
2024-04-14 02:32:19 +0900

    [0414] Template:

---
+++ BackEnd/07. Spring Advanced/practice/advanced/src/main/java/hello/advanced/app/v5/OrderRepositoryV5.java
@@ -0,0 +1,36 @@
+package hello.advanced.app.v5;
+
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV5 {
+
+    private final LogTrace trace;
+
+    public void save(String itemId) {
+        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
+            @Override
+            protected Void call() {
+                if (itemId.equals("ex")) {
+                    throw new IllegalStateException("예외 발생!");
+                }
+                sleep(1000);
+                return null;
+            }
+        };
+        template.execute("OrderRepository.save()");
+
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ BackEnd/07. Spring Advanced/practice/advanced/src/main/java/hello/advanced/app/v5/OrderServiceV5.java
@@ -0,0 +1,24 @@
+package hello.advanced.app.v5;
+
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV5 {
+    private final OrderRepositoryV5 orderRepository;
+    private final LogTrace trace;
+
+    public void orderItem(String itemId) {
+        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
+            @Override
+            protected Void call() {
+                orderRepository.save(itemId);
+                return null;
+            }
+        };
+        template.execute("OrderService.orderItem()");
+    }
+}
