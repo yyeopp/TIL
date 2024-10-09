5d097578b973a303a51a9e35327358c21d32f2d1
yyeopp <letsksy@naver.com>
2024-10-09 18:47:29 +0900

    [1009] Bean PostProcessor

---
+++ BackEnd/07. Spring Advanced/practice/proxy/src/main/java/hello/proxy/config/v4_postprocessor/postprocessor/PackageLogTracePostProcessor.java
@@ -0,0 +1,32 @@
+package hello.proxy.config.v4_postprocessor.postprocessor;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+@RequiredArgsConstructor
+@Slf4j
+public class PackageLogTracePostProcessor implements BeanPostProcessor {
+
+    private final String basePackage;
+    private final Advisor advisor;
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        log.info("param beanName = {}, bean = {}", beanName, bean.getClass());
+
+        String packageName = bean.getClass().getPackageName();
+        if (!packageName.startsWith(basePackage)) {
+            return bean;
+        }
+
+        ProxyFactory proxyFactory = new ProxyFactory(bean);
+        proxyFactory.addAdvisor(advisor);
+        Object proxy = proxyFactory.getProxy();
+        log.info("create proxy : target = {}, proxy = {}", bean.getClass(), proxy.getClass());
+        return proxy;
+    }
+}
