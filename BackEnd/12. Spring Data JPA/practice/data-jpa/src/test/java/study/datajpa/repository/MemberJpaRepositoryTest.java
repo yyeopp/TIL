3282a1bb151332fcbd2e90fe01e88eb7b3e564a5
yyeopp <letsksy@naver.com>
2025-06-09 15:52:22 +0900

    [0609] Spring Data JPA

---
+++ BackEnd/12. Spring Data JPA/practice/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,61 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        final Member savedMember = memberJpaRepository.save(member);
+        final Member findMember = memberJpaRepository.find(savedMember.getId());
+
+        assertEquals(savedMember.getId(), findMember.getId());
+        assertEquals(savedMember.getUsername(), findMember.getUsername());
+        assertEquals(findMember, member);
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+
+        assertEquals(findMember1, member1);
+        assertEquals(findMember2, member2);
+
+        List<Member> all = memberJpaRepository.findAll();
+        assertEquals(all.size(), 2);
+
+        long count = memberJpaRepository.count();
+        assertEquals(count, 2);
+
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long deletedCount = memberJpaRepository.count();
+        assertEquals(deletedCount, 0);
+    }
+
+
+}
---
+++ BackEnd/12. Spring Data JPA/practice/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,58 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberB");
+        final Member savedMember = memberRepository.save(member);
+        final Member findMember = memberRepository.findById(savedMember.getId()).get();
+        assertEquals(savedMember.getId(), findMember.getId());
+        assertEquals(savedMember.getUsername(), findMember.getUsername());
+        assertEquals(findMember, member);
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        assertEquals(findMember1, member1);
+        assertEquals(findMember2, member2);
+
+        List<Member> all = memberRepository.findAll();
+        assertEquals(all.size(), 2);
+
+        long count = memberRepository.count();
+        assertEquals(count, 2);
+
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long deletedCount = memberRepository.count();
+        assertEquals(deletedCount, 0);
+    }
+}
